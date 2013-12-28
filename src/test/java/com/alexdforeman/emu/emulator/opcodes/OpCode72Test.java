f739fe774439804427c1da6eedbd3c40bab4d132
alexdforeman <alexdforeman@gmail.com>
2013-12-28 19:57:23 +0000

    Added OpCode 0x72

---
+++ src/test/java/com/alexdforeman/emu/emulator/opcodes/OpCode72Test.java
@@ -0,0 +1,14 @@
+package com.alexdforeman.emu.emulator.opcodes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OpCode72Test {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
