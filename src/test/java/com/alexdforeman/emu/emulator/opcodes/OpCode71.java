35c51914c54d20a2016c4ce772e1fc69ced6d275
alexdforeman <alexdforeman@gmail.com>
2013-12-28 19:51:34 +0000

    Added OpCode 0x71

--- src/test/java/com/alexdforeman/emu/emulator/opcodes/OpCode71.java
+++ src/test/java/com/alexdforeman/emu/emulator/opcodes/OpCode71.java
@@ -3,20 +3,20 @@ package com.alexdforeman.emu.emulator.opcodes;
 import com.alexdforeman.emu.emulator.State;
 
 /**
- * OpCode 0x70 MOV M,B.
- * Move B to M or the HL pair.
+ * OpCode 0x71 MOV M,C.
+ * Move C to M or the HL pair.
  * @author http://github.com/alexdforeman
  */
-public class OpCode70 implements IOpCode {
+public class OpCode71 implements IOpCode {
 
     /* (non-Javadoc)
      * @see com.alexdforeman.emu.emulator.opcodes.IOpcode#exec(com.alexdforeman.emu.emulator.State)
      */
     @Override
     public final void exec(State state_) {
-        int b = state_.getB();
-        state_.setH((b >>> _SHIFT) & _UNSIGNED_MASK);
-        state_.setL(b & _UNSIGNED_MASK);
+        int c = state_.getC();
+        state_.setH((c >>> _SHIFT) & _UNSIGNED_MASK);
+        state_.setL(c & _UNSIGNED_MASK);
         state_.setPc(state_.getPc() + 1);
     }
 }
